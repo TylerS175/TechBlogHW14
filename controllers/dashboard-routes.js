61b0ab15ea575545c5324c0daab141810c013c5b
Tyler Shifflett <tylershifflett@Tylers-MacBook-Pro.local>
2022-07-22 22:20:17 -0400

    Spelling Correction HomeRoutes and Launching SQL

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -50,7 +50,7 @@ router.get('/edit/:id', withAuth, (req, res) => {
       'post_url',
       'title',
       'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      'comment_text',
     ],
     include: [
       {
